92a8fc525c0631a68624e02df0baefe703362912
Farzaneh Rahmdel <66174397+Farzaneh-ra@users.noreply.github.com>
2026-03-09 22:23:36 -0400

    calculate daily returns

--- financial_data.py
+++ financial_data.py
@@ -2,4 +2,5 @@ import pandas as pd
 import yfinance as yf
 data = yf.download("AAPL", start="2020-01-01")
 print(data.head())
-print(data.tail())
+data["returns"] = data['Close'].pct_change()
+print(data[['Close','returns']].tail(10))
